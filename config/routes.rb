bbbc7d25faa01d8b2e5fb2f14a7ce4752a18c912
therunningagent <luistjr@gmail.com>
2020-12-01 20:10:07 -0500

    index + new form

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :recipes
   resources :meal_kits
   resources :lifestyles
-  resources :surveys
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
