25f55b368eeee808668f8c829f8be8ef85e164c7
antrubbo <anthony.rubbo@gmail.com>
2020-12-03 13:51:38 -0500

    work on survey_answers action

--- app/controllers/survey_results_controller.rb
+++ app/controllers/survey_results_controller.rb
@@ -19,32 +19,15 @@ class SurveyResultsController < ApplicationController
         score << params[:survey][:answer_5]
         score << params[:survey][:answer_6]
         score << params[:survey][:answer_7]
-        survey_count
+        
+        athletic = score.select {|w| w == "athletic"}.count
+        healthy = score.select {|w| w == "healthy"}.count
+        foodie = score.select {|w| w == "foodie"}.count
+        hungry = score.select {|w| w == "hungry"}.count
+        byebug
     end
 
-    def survey_count
-        a_1 = survey_answers.count("athletic")
-        a_2 = survey_answers.count("healthy")
-        a_3 = survey_answers.count("foodie")
-        a_4 = survey_answers.count("hungry")
-        complete_answers = [a_1, a_2, a_3, a_4]
-        return complete_answers
-        # byebug
-    end 
-
-    # def max_complete_answers
-    #     lifestyle_choice = survey_count.max
-
-    #     if lifestyle_choice == a_1
-    #         lifestyle_id =
-    #     elsif lifestyle_choice == a_2
-
-    #     elsif lifestyle_choice == a_3
-
-    #     else lifestyle_choice == a_4
-    # end 
-
-    # def lifestyle_choice
-    # end 
+    def assign_lifestyle
+    end
     
 end
